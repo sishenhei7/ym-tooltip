2776dffc52327953b178c5b84e01d15f766a6fab
杨舟 <yangzhou@yimian.com.cn>
2019-07-08 17:22:08 +0800

    initial commit

---
+++ lib/YmTooltip.vue
@@ -0,0 +1,32 @@
+<template>
+  <el-tooltip
+    ref="tooltip"
+    v-bind="$attrs"
+  >
+    <template slot="content">
+      <slot name="content" />
+    </template>
+    <slot />
+  </el-tooltip>
+</template>
+<script>
+export default {
+  name: 'YmTooltip',
+  mounted() {
+    window.addEventListener('touchmove', this.hideTip);
+    if (this.$el.nodeType === 1) {
+      this.$el.classList.add('ym-tooltip');
+    }
+  },
+  beforeDestroy() {
+    window.removeEventListener('touchmove', this.hideTip);
+  },
+  methods: {
+    hideTip() {
+      if (this.$el.blur) {
+        this.$el.blur();
+      }
+    },
+  },
+};
+</script>
